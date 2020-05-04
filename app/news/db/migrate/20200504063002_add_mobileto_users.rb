9bff35b096ca0d456200160590fa864be2ff9758
rmit-s3668759-casey-coulter <s3668759@student.rmit.edu.au>
2020-05-04 16:53:41 +1000

    added mobile to database and footer to each page

---
+++ app/news/db/migrate/20200504063002_add_mobileto_users.rb
@@ -0,0 +1,5 @@
+class AddMobiletoUsers < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :mobile, :integer
+  end
+end
--- app/news/db/schema.rb
+++ app/news/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_105331) do
+ActiveRecord::Schema.define(version: 2020_05_04_063002) do
 
   create_table "discussions", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_05_03_105331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "mobile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
