101f092f7ca4682e749253c8fa02731dc24d48ae
rmit-s3668759-casey-coulter <s3668759@student.rmit.edu.au>
2020-05-04 15:04:06 +1000

    authors can only edit their own discussions/reply

--- app/news/app/helpers/application_helper.rb
+++ app/news/app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # This is a plugin that strips markdown from a field of text
   require 'redcarpet/render_strip'
 
   # Check what role the current user has
@@ -6,19 +7,22 @@ module ApplicationHelper
     current_user && current_user.has_role?(role)
   end
 
+  # Majority of the below code was not mine
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code,language).div
+      CodeRay.scan(code, language).div
     end
   end
 
   def markdown(text)
     coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+    # There are many more options you can use here, these are the ones
+    # I found on a forum post
     options = {
       fenced_code_blocks: true,
-      no_intra_emphasis: true,
-      autolink: true,
-      lax_html_blocks: true
+      no_intra_emphasis:  true,
+      autolink:           true,
+      lax_html_blocks:    true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
