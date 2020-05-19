3a7055b7be6848b994d9bbb33b28dd4c512a9022
jryan2605 <jackryan1998@hotmail.com>
2020-05-19 17:02:51 +1000

    need to fix last_seen_at

--- app/news/db/migrate/20200509052729_add_last_seen_at_to_users.rb
+++ app/news/db/migrate/20200509052729_add_last_seen_at_to_users.rb
@@ -1,5 +1,6 @@
 class AddLastSeenAtToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :last_seen_at, :datetime
+    add_column :users, :last_seen_at, :datetime, :default => DateTime.now
+    change_column  :users, :last_seen_at, :datetime, :default => DateTime.now
   end
 end
