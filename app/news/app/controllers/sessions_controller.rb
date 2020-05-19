f614758d0e5b491c9281ebd537eff0116f8b8380
jryan2605 <jackryan1998@hotmail.com>
2020-05-19 18:06:06 +1000

    recent mem done

--- app/news/app/controllers/sessions_controller.rb
+++ app/news/app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       remember user
+      user.update_attribute(:last_seen_at, Time.now)
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
     else
